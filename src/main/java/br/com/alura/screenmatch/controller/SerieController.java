bddae6a55f8de938fea7070810c2d6dada24d54d
Wev kleyton <kleyton.cpd@gmnail.com>
2026-01-12 14:30:03 -0300

    Modificações para mostrar series mais recentes e exeibir series do banco

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -6,20 +6,31 @@ import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService servico;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries(){
         return  servico.obterTodasAsSeries();
     }
 
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return  servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamentos(){
+        return servico.obterLancamentos();
+    }
 }
